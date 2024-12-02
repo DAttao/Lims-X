6931e30694bb58ca8a633b177e2a31397a2e722a
DAttao <93329795+DAttao@users.noreply.github.com>
2024-12-02 17:43:22 +0800

    初始化

---
+++ Lims-web/src/main/java/com/example/limsweb/LimsWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.limsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimsWebApplication.class, args);
+    }
+
+}
