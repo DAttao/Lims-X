85318be69dac258382ff7321a68bc25b14b1e80c
zt <3041198211@qq.com>
2024-12-02 18:23:24 +0800

    1

--- Lims-web/src/main/java/com/example/limsweb/LimsWebApplication.java
+++ Lims-web/src/main/java/com/example/limsweb/LimsWebApplication.java
@@ -8,6 +8,7 @@ public class LimsWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LimsWebApplication.class, args);
+        System.out.println("asdasdsa");
     }
 
 }
